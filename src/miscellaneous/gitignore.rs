8434fc17423a13482a13f1a76356a69b391fabbe
Cyberunner23 <narutoxela@gmail.com>
2016-12-24 22:54:29 -0500

    Moved Gitignore to extension trait and made docs

--- src/miscellaneous/gitignore.rs
+++ src/miscellaneous/gitignore.rs
@@ -5,6 +5,13 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
+//! # Gitignore
+//!
+//! These are the responses and API call functions related
+//! to the gitignore endpoints of the API.
+//!
+//! Reference: https://developer.github.com/v3/activity/watching/
+
 use hyper::header::{Accept, qitem};
 use hyper::mime::{Mime, TopLevel, SubLevel};
 
@@ -12,8 +19,7 @@ use client::Client;
 use error;
 use utils;
 
-///Reference: https://developer.github.com/v3/gitignore/
-
+///The response of the non-raw single template request.
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct GitignoreTemplate {
     pub name:   String,
@@ -21,26 +27,53 @@ pub struct GitignoreTemplate {
 }
 
 ////////////////////////////////////////////////////////////
-//                       Functions                        //
+//                    Extension Trait                     //
 ////////////////////////////////////////////////////////////
 
-///Reference: https://developer.github.com/v3/gitignore/#listing-available-templates
-pub fn get_gitignore_templates(client: &mut Client) -> Result<Vec<String>, error::Error> {
-    utils::request_endpoint(client, "/gitignore/templates".into())
+pub trait GitignoreExt {
+
+    /// \[[Reference](https://developer.github.com/v3/gitignore/#listing-available-templates)\]
+    /// Returns the list available templates when creating a
+    /// repository.
+    /// ## Endpoint:
+    /// GET /gitignore/templates
+    fn get_gitignore_templates(&mut self) -> Result<Vec<String>, error::Error>;
+
+    /// \[[Reference](https://developer.github.com/v3/gitignore/#get-a-single-template)\]
+    /// Returns a single template.
+    /// ## Endpoint:
+    /// GET /gitignore/templates/:name
+    /// ## Parameters:
+    /// * `name`: Name of/the language template requested
+    fn get_gitignore_templates_name(&mut self, name: &String) -> Result<GitignoreTemplate, error::Error>;
+
+    /// \[[Reference](https://developer.github.com/v3/gitignore/#get-a-single-template)\]
+    /// Returns a single template in raw form.
+    /// ## Endpoint:
+    /// GET /gitignore/templates/:name
+    /// ## Parameters:
+    /// * `name`: Name of/the language template requested
+    fn get_gitignore_templates_name_raw(&mut self, name: &String) -> Result<String, error::Error>;
 }
 
-///Reference: https://developer.github.com/v3/gitignore/#get-a-single-template
-pub fn get_gitignore_templates_name(client: &mut Client, name: &String) -> Result<GitignoreTemplate, error::Error> {
-    utils::request_endpoint(client, format!("/gitignore/templates/{}", name))
-}
+impl GitignoreExt for Client {
 
-pub fn get_gitignore_templates_name_raw(client: &mut Client, name: &String) -> Result<String, error::Error> {
+    fn get_gitignore_templates(&mut self) -> Result<Vec<String>, error::Error> {
+        utils::request_endpoint(self, "/gitignore/templates".into())
+    }
 
-    let mut header = client.get_default_headers();
-    header.remove::<Accept>();
-    header.set(Accept(vec![qitem(Mime(TopLevel::Application, SubLevel::Ext("vnd.github.v3.raw".to_string()), vec![]))]));
+    fn get_gitignore_templates_name(&mut self, name: &String) -> Result<GitignoreTemplate, error::Error> {
+        utils::request_endpoint(self, format!("/gitignore/templates/{}", name))
+    }
 
-    utils::request_endpoint_with_headers(client, format!("/gitignore/templates/{}", name), Some(header))
+    fn get_gitignore_templates_name_raw(&mut self, name: &String) -> Result<String, error::Error> {
+
+        let mut header = self.get_default_headers();
+        header.remove::<Accept>();
+        header.set(Accept(vec![qitem(Mime(TopLevel::Application, SubLevel::Ext("vnd.github.v3.raw".to_string()), vec![]))]));
+
+        utils::request_endpoint_with_headers(self, format!("/gitignore/templates/{}", name), Some(header))
+    }
 }
 
 //TODO: TESTS
