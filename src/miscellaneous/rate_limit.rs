74db6fc44b885f87fac91ea5fede3e44fe8f59db
Cyberunner23 <narutoxela@gmail.com>
2016-12-26 00:04:01 -0500

    Moved RateLimit to extension trait and made docs

--- src/miscellaneous/rate_limit.rs
+++ src/miscellaneous/rate_limit.rs
@@ -5,25 +5,32 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
+//! # Rate Limit
+//!
+//! These are the responses and API call functions related
+//! to the rate_limit endpoint of the API.
+//!
+//! Reference: https://developer.github.com/v3/rate_limit/
+
 use client::Client;
 use error;
 use utils;
 
-///Reference: https://developer.github.com/v3/rate_limit/
-
-/// The response to GET /rate_limit
+/// The response to rate_limit endpoint.
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct RateLimit {
     pub resources: RateLimitResources
     //rate omitted since it is deprecated and will ne removed in the next version of the API
 }
 
+///Sub-component of the rate_limit response.
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct RateLimitResources {
     pub core:   RateLimitElement,
     pub search: RateLimitElement
 }
 
+///Sub-component of the rate_limit response.
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct RateLimitElement {
     pub limit:     u64,
@@ -32,11 +39,21 @@ pub struct RateLimitElement {
 }
 
 ////////////////////////////////////////////////////////////
-//                       Functions                        //
+//                    Extension Trait                     //
 ////////////////////////////////////////////////////////////
 
-pub fn get_rate_limit(client: &mut Client) -> Result<RateLimit, error::Error> {
-    utils::request_endpoint(client, "/rate_limit".into())
+pub trait RateLimitExt {
+    /// \[[Reference](https://developer.github.com/v3/rate_limit/#get-your-current-rate-limit-status)\]
+    /// Returns the current rate limit status.
+    /// ## Endpoint:
+    /// GET /rate_limit
+    fn get_rate_limit(&mut self) -> Result<RateLimit, error::Error>;
+}
+
+impl RateLimitExt for Client {
+    fn get_rate_limit(&mut self) -> Result<RateLimit, error::Error> {
+        utils::request_endpoint(self, "/rate_limit".into())
+    }
 }
 
 //TODO: TESTS
